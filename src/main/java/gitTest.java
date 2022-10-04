1d6399597e1bc123f424fdf065ef20e473688bff
lp <lp@sicnu.com>
2022-10-04 15:24:12 +0800

    master commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -6,6 +6,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("idea git hello!!");
         System.out.println("idea git hello!!");
-        System.out.println("hot fix branch!!");
+        System.out.println("master git!!");
     }
 }
